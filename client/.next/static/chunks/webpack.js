2455a363254e1385082a30b8b25fa940d499ecea
SidZhoop <siddharth@zhoop.in>
2023-06-25 19:24:39 +0530

    api func created

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ca09b7a7f1281923"; }
+/******/ 		__webpack_require__.h = function() { return "97c3c4f9226d999e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
