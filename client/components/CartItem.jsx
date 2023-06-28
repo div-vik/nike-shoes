79629f46bf4481273933e6e91fa6926501ad0479
SidZhoop <siddharth@zhoop.in>
2023-06-28 20:39:32 +0530

    cart page completed

--- client/components/CartItem.jsx
+++ client/components/CartItem.jsx
@@ -1,18 +1,33 @@
+import { removeFromCart, updateCart } from "@/store/cartSlice";
+import Image from "next/image";
 import React from "react";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import { useDispatch } from "react-redux";
 
-const CartItem = () => {
+const CartItem = ({ data }) => {
+  const p = data.attributes;
+
+  const dispatch = useDispatch();
+
+  const updateCartItem = (e, key) => {
+    let payload = {
+      key,
+      val: key === "quantity" ? parseInt(e.target.value) : e.target.value,
+      id: data.id,
+    };
+    dispatch(updateCart(payload));
+  };
+
   return (
     <div className="flex py-5 gap-3 md:gap-5 border-b">
       {/* IMAGE START */}
       <div className="shrink-0 aspect-square w-[50px] md:w-[120px]">
-        {/* <Image
+        <Image
           src={p.thumbnail.data.attributes.url}
           alt={p.name}
           width={120}
           height={120}
-        /> */}
-        <img src="product-1.webp" alt="image" />
+        />
       </div>
       {/* IMAGE END */}
 
@@ -20,62 +35,68 @@ const CartItem = () => {
         <div className="flex flex-col md:flex-row justify-between">
           {/* PRODUCT TITLE */}
           <div className="text-lg md:text-2xl font-semibold text-black/[0.8]">
-            Jordan Retro 6 G
+            {p.name}
           </div>
 
           {/* PRODUCT SUBTITLE */}
           <div className="text-sm md:text-md font-medium text-black/[0.5] block md:hidden">
-            Men&apos;s Golf Shoes
+            {p.subtitle}
           </div>
 
           {/* PRODUCT PRICE */}
           <div className="text-sm md:text-md font-bold text-black/[0.5] mt-2">
-            MRP : ₹ 19 695.00
+            MRP : &#8377;{p.price}
           </div>
         </div>
 
         {/* PRODUCT SUBTITLE */}
         <div className="text-md font-medium text-black/[0.5] hidden md:block">
-          Men&apos;s Golf Shoes
+          {p.subtitle}
         </div>
 
         <div className="flex items-center justify-between mt-4">
           <div className="flex items-center gap-2 md:gap-10 text-black/[0.5] text-sm md:text-md">
             <div className="flex items-center gap-1">
               <div className="font-semibold">Size</div>
-              <select className="hover:text-black outline-none">
-                <option value="1">UK 6</option>
-                <option value="1">UK 6</option>
-                <option value="1">UK 6</option>
-                <option value="1">UK 6</option>
-                <option value="1">UK 6</option>
-                <option value="1">UK 6</option>
-                <option value="1">UK 6</option>
-                <option value="1">UK 6</option>
-                <option value="1">UK 6</option>
-                <option value="1">UK 6</option>
-                <option value="1">UK 6</option>
+              <select
+                className="hover:text-black cursor-pointer outline-none"
+                onChange={(e) => updateCartItem(e, "selectedSize")}
+              >
+                {p.size.data.map((item, id) => {
+                  return (
+                    <option
+                      key={id}
+                      value={item.size}
+                      disabled={!item.enabled ? true : false}
+                      selected={data.selectedSize === item.size}
+                    >
+                      {item.size}
+                    </option>
+                  );
+                })}
               </select>
             </div>
 
             <div className="flex items-center gap-1">
               <div className="font-semibold">Quantity</div>
-              <select className="hover:text-black outline-none">
-                <option value="1">6</option>
-                <option value="1">6</option>
-                <option value="1">6</option>
-                <option value="1">6</option>
-                <option value="1">6</option>
-                <option value="1">6</option>
-                <option value="1">6</option>
-                <option value="1">6</option>
-                <option value="1">6</option>
-                <option value="1">6</option>
-                <option value="1">6</option>
+              <select
+                className="hover:text-black cursor-pointer outline-none"
+                onChange={(e) => updateCartItem(e, "quantity")}
+              >
+                {Array.from({ length: 10 }, (_, i) => i + 1).map((q, i) => {
+                  return (
+                    <option key={i} selected={data.quantity === q} value={q}>
+                      {q}
+                    </option>
+                  );
+                })}
               </select>
             </div>
           </div>
-          <RiDeleteBin6Line className="cursor-pointer text-black/[0.5] hover:text-black text-[16px] md:text-[20px]" />
+          <RiDeleteBin6Line
+            onClick={() => dispatch(removeFromCart({ id: data.id }))}
+            className="cursor-pointer text-black/[0.5] hover:text-black text-[16px] md:text-[20px]"
+          />
         </div>
       </div>
     </div>
